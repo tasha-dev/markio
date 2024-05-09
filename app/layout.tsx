86755abbeedbdf482bf1e0edd21134dc6c4f360f
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2024-05-09 07:44:54 +0330

    added background images

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ import {rootLayoutType} from "@/types";
 import {ReactNode} from "react";
 import {siteConfig} from "@/configs";
 import {Metadata} from "next";
+import Image from 'next/image';
+import bgLightImage from '@/images/bg-light.jpg';
+import bgDarkImage from '@/images/bg-dark.jpg';
 
 // Defining fonts
 const fontSans = Inter({
@@ -33,8 +36,13 @@ export const metadata:Metadata = {
 // Creating and exporting root layout component as default
 export default function RootLayout({children}: rootLayoutType):ReactNode {
     return (
-        <html lang="en" suppressHydrationWarning>
-            <body className={cn("min-h-screen", fontSans.className)}>
+        <html lang="en" suppressHydrationWarning className={'dark'}>
+            <body className={cn(
+                "min-h-screen dark:bg-black bg-white",
+                fontSans.className)
+            }>
+                <Image src={bgLightImage.src} alt={'Background Image'} width={1920} height={1080} className={'fixed top-0 left-0 -z-10 w-screen h-screen object-cover transition-all duration-500 dark:opacity-0 opacity-100'} />
+                <Image src={bgDarkImage.src} alt={'Background Image'} width={1920} height={1080} className={'fixed top-0 left-0 -z-10 w-screen h-screen object-cover transition-all duration-500 dark:opacity-100 opacity-0'} />
                 {children}
             </body>
         </html>
