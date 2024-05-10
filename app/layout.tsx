15327a5fdbac84ff19ba6e9c308725d5c745147c
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2024-05-10 16:14:58 +0330

    added add feature to file menu

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,7 @@ import bgDarkImage from '@/images/bg-dark.jpg';
 import TopMenu from "@/components/topMenu";
 import FileMenu from "@/components/fileMenu";
 import Tiptap from "@/components/tiptap";
+import {Toaster} from "@/components/ui/sonner";
 
 // Defining fonts
 const fontSans = Inter({
@@ -46,6 +47,7 @@ export default function RootLayout({children}: rootLayoutType):ReactNode {
             }>
                 <Image src={bgLightImage.src} alt={'Background Image'} width={1920} height={1080} className={'fixed top-0 left-0 -z-10 w-screen h-screen object-cover transition-all duration-500 dark:opacity-0 opacity-100'} />
                 <Image src={bgDarkImage.src} alt={'Background Image'} width={1920} height={1080} className={'fixed top-0 left-0 -z-10 w-screen h-screen object-cover transition-all duration-500 dark:opacity-100 opacity-0'} />
+                <Toaster />
                 <div className={'flex flex-col overflow-hidden h-dvh'}>
                     <TopMenu />
                     <div className={'lg:grid grid-cols-4 h-[calc(100%-2.5rem)]'}>
--- components/fileMenu.tsx
+++ components/fileMenu.tsx
@@ -7,14 +7,43 @@ import {ReactNode} from "react";
 import File from "@/components/ui/file";
 import {useFileMenu, useFiles} from "@/app/store";
 import {cn} from "@/lib/utils";
+import {Button} from '@/components/ui/button';
+import {Plus} from 'lucide-react';
+import {Popover, PopoverContent, PopoverTrigger} from "@/components/ui/popover";
+import z from 'zod';
+import {SubmitHandler, useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {Form, FormControl, FormField, FormItem, FormLabel} from "@/components/ui/form";
+import {Input} from "@/components/ui/input";
+import {toast} from "sonner";
+
+// Defining schema for new file form and type of it
+const formSchema = z.object({fileName: z.string().min(3).max(20)})
+type formType = z.infer<typeof formSchema>
 
 // Creating and exporting file menu component as default
 export default function FileMenu():ReactNode {
-    // Getting data of opened or closed from zustand store
-    const {opened, activeFile, changeOpen} = useFileMenu()
+    // Defining react hook form
+    const form = useForm<formType>({resolver: zodResolver(formSchema)})
 
-    // Gettind data of files from zustand
-    const {files} = useFiles();
+    // Getting data of opened or closed from zustand store
+    const {opened, activeFile, changeOpen, changeActive} = useFileMenu()
+
+    // Getting data of files from zustand
+    const {files, add} = useFiles();
+
+    // Defining a function to handle submit of new file form
+    const onSubmitHandler:SubmitHandler<formType> = ({fileName}) => {
+        if (files.map(item => item.name).includes(fileName)) {
+            form.setError('fileName', {
+                message: 'The file name already exists',
+            });
+        } else {
+            add(fileName, `<h1>${fileName}</h1>`);
+            changeActive(fileName);
+            toast('The file is added');
+        }
+    }
 
     // Returning JSX
     return (
@@ -32,11 +61,35 @@ export default function FileMenu():ReactNode {
                     'lg:backdrop-blur-2xl lg:dark:bg-black/20 lg:bg-white/20 dark:bg-black bg-white h-full lg:border-r lg:dark:border-r-white/20 lg:border-r-black/20 overflow-auto lg:static fixed top-0 lg:z-0 z-50 transition-all duration-500 lg:w-auto w-[75%]'
                 )}
             >
+                <Popover>
+                    <PopoverTrigger asChild>
+                        <div className={'p-1'}>
+                            <Button size={'icon'} variant={'ghost'}>
+                                <Plus className={'w-4 h-4'}/>
+                            </Button>
+                        </div>
+                    </PopoverTrigger>
+                    <PopoverContent>
+                        <Form {...form}>
+                            <form action="#" onSubmit={form.handleSubmit(onSubmitHandler)}>
+                                <FormField control={form.control} name={'fileName'} render={({field}) => (
+                                    <FormItem>
+                                        <FormLabel>File Name</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder={"Without .md extension at end"} {...field} />
+                                        </FormControl>
+                                    </FormItem>
+                                )}/>
+                                <Button type={'submit'} className={'w-full mt-[20px]'}>Create</Button>
+                            </form>
+                        </Form>
+                    </PopoverContent>
+                </Popover>
                 {
                     (files.length === 0)
                         ? (
                             <div className={'p-[20px]'}>
-                                <h1 className={'text-[20px] font-medium dark:text-white text-black text-center'}>There is nothing to show</h1>
+                            <h1 className={'text-[20px] font-medium dark:text-white text-black text-center'}>There is nothing to show</h1>
                             </div>
                         ) : <ul className={'flex flex-col'}>
                             {
