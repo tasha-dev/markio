123c435d572cb084becda3aa59ca58eb0eff8de0
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2024-05-11 10:48:37 +0330

    improved time complexity in showing each file in editor

--- components/tiptap.tsx
+++ components/tiptap.tsx
@@ -25,7 +25,6 @@ import {
     List, CodeXml,
 } from "lucide-react";
 import {tiptapType} from "@/types";
-import {cn} from "@/lib/utils";
 
 // Creating and exporting tiptap component (editor) as default
 export default function Tiptap({isOpened}:tiptapType):ReactNode {
@@ -43,59 +42,59 @@ export default function Tiptap({isOpened}:tiptapType):ReactNode {
         onUpdate: (e) => (editor?.getHTML()) ? setContent(activeFile, editor.getHTML()) : setContent(activeFile, '')
     })
 
-    // Returning JSX
-    return (
-        <Container className={cn(
-            'editor',
-            (isOpened) ? 'block' : 'hidden'
-        )}>
-            {
-                editor && (
-                    <BubbleMenu className={'dark:bg-black bg-white rounded-lg p-[5px] border dark:border-white/20 border-black/20'} editor={editor} tippyOptions={{ duration: 500 }}>
-                        <ToggleGroup type="multiple">
-                            <HoverDropDown icon={<Pilcrow className={'w-4 h-4'}/>}>
-                                <span className={'text-sm text-muted-foreground mb-[5px] block'}>HIERARCHY</span>
-                                <Button variant={'ghost'} onClick={() => editor.chain().focus().toggleHeading({level: 1}).run()} className={'flex items-center gap-2 w-full'}>
-                                    <Heading1 className={'w-4 h-4 shrink-0'}/>
-                                    <span className={'truncate block w-full text-start'}>Heading 1</span>
-                                </Button>
-                                <Button variant={'ghost'} onClick={() => editor.chain().focus().toggleHeading({level: 2}).run()} className={'flex items-center gap-2 w-full'}>
-                                    <Heading2 className={'w-4 h-4 shrink-0'}/>
-                                    <span className={'truncate block w-full text-start'}>Heading 2</span>
-                                </Button>
-                                <Button variant={'ghost'} onClick={() => editor.chain().focus().toggleHeading({level: 3}).run()} className={'flex items-center gap-2 w-full'}>
-                                    <Heading3 className={'w-4 h-4 shrink-0'}/>
-                                    <span className={'truncate block w-full text-start'}>Heading 3</span>
-                                </Button>
-                                <Separator orientation={'horizontal'} className={'my-4'}/>
-                                <span className={'text-sm text-muted-foreground mb-[5px] block'}>LISTS</span>
-                                <Button variant={'ghost'} onClick={() => editor.chain().focus().toggleBulletList().run()} className={'flex items-center gap-2 w-full'}>
-                                    <List className={'w-4 h-4 shrink-0'} />
-                                    <span className={'truncate block w-full text-start'}>Bullet list</span>
-                                </Button>
-                                <Button variant={'ghost'} onClick={() => editor.chain().focus().toggleOrderedList().run()} className={'flex items-center gap-2 w-full'}>
-                                    <AlignJustify className={'w-4 h-4 shrink-0'} />
-                                    <span className={'truncate block w-full text-start'}>Numbred list</span>
-                                </Button>
-                            </HoverDropDown>
-                            <Separator orientation={'vertical'}/>
-                            <ToggleGroupItem onClick={() => editor.chain().focus().toggleBold().run()} value="bold" aria-label="Toggle bold">
-                                <Bold className="h-4 w-4" />
-                            </ToggleGroupItem>
-                            <ToggleGroupItem onClick={() => editor.chain().focus().toggleItalic().run()} value="italic" aria-label="Toggle italic">
-                                <Italic className="h-4 w-4" />
-                            </ToggleGroupItem>
-                            <ToggleGroupItem onClick={() => editor.chain().focus().toggleStrike().run()} value="underline" aria-label="Toggle underline">
-                                <Strikethrough className="h-4 w-4" />
-                            </ToggleGroupItem>
-                            <ToggleGroupItem onClick={() => editor.chain().focus().toggleCodeBlock().run()} value="underline" aria-label="Toggle underline">
-                                <CodeXml className="h-4 w-4" />
-                            </ToggleGroupItem>
-                        </ToggleGroup>
-                    </BubbleMenu>
-                )
-            }
-            <EditorContent editor={editor}/>
-        </Container>
-    );
+    // Conditional rendering
+    if (!isOpened) {return false}
+    else {
+        return (
+            <Container className={'editor'}>
+                {
+                    editor && (
+                        <BubbleMenu className={'dark:bg-black bg-white rounded-lg p-[5px] border dark:border-white/20 border-black/20'} editor={editor} tippyOptions={{ duration: 500 }}>
+                            <ToggleGroup type="multiple">
+                                <HoverDropDown icon={<Pilcrow className={'w-4 h-4'}/>}>
+                                    <span className={'text-sm text-muted-foreground mb-[5px] block'}>HIERARCHY</span>
+                                    <Button variant={'ghost'} onClick={() => editor.chain().focus().toggleHeading({level: 1}).run()} className={'flex items-center gap-2 w-full'}>
+                                        <Heading1 className={'w-4 h-4 shrink-0'}/>
+                                        <span className={'truncate block w-full text-start'}>Heading 1</span>
+                                    </Button>
+                                    <Button variant={'ghost'} onClick={() => editor.chain().focus().toggleHeading({level: 2}).run()} className={'flex items-center gap-2 w-full'}>
+                                        <Heading2 className={'w-4 h-4 shrink-0'}/>
+                                        <span className={'truncate block w-full text-start'}>Heading 2</span>
+                                    </Button>
+                                    <Button variant={'ghost'} onClick={() => editor.chain().focus().toggleHeading({level: 3}).run()} className={'flex items-center gap-2 w-full'}>
+                                        <Heading3 className={'w-4 h-4 shrink-0'}/>
+                                        <span className={'truncate block w-full text-start'}>Heading 3</span>
+                                    </Button>
+                                    <Separator orientation={'horizontal'} className={'my-4'}/>
+                                    <span className={'text-sm text-muted-foreground mb-[5px] block'}>LISTS</span>
+                                    <Button variant={'ghost'} onClick={() => editor.chain().focus().toggleBulletList().run()} className={'flex items-center gap-2 w-full'}>
+                                        <List className={'w-4 h-4 shrink-0'} />
+                                        <span className={'truncate block w-full text-start'}>Bullet list</span>
+                                    </Button>
+                                    <Button variant={'ghost'} onClick={() => editor.chain().focus().toggleOrderedList().run()} className={'flex items-center gap-2 w-full'}>
+                                        <AlignJustify className={'w-4 h-4 shrink-0'} />
+                                        <span className={'truncate block w-full text-start'}>Numbred list</span>
+                                    </Button>
+                                </HoverDropDown>
+                                <Separator orientation={'vertical'}/>
+                                <ToggleGroupItem onClick={() => editor.chain().focus().toggleBold().run()} value="bold" aria-label="Toggle bold">
+                                    <Bold className="h-4 w-4" />
+                                </ToggleGroupItem>
+                                <ToggleGroupItem onClick={() => editor.chain().focus().toggleItalic().run()} value="italic" aria-label="Toggle italic">
+                                    <Italic className="h-4 w-4" />
+                                </ToggleGroupItem>
+                                <ToggleGroupItem onClick={() => editor.chain().focus().toggleStrike().run()} value="underline" aria-label="Toggle underline">
+                                    <Strikethrough className="h-4 w-4" />
+                                </ToggleGroupItem>
+                                <ToggleGroupItem onClick={() => editor.chain().focus().toggleCodeBlock().run()} value="underline" aria-label="Toggle underline">
+                                    <CodeXml className="h-4 w-4" />
+                                </ToggleGroupItem>
+                            </ToggleGroup>
+                        </BubbleMenu>
+                    )
+                }
+                <EditorContent editor={editor}/>
+            </Container>
+        );
+    }
 }
