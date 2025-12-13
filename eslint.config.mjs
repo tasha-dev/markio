9a4bc583440df9597c806784d9a1169681b3d1a0
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2025-12-13 12:19:08 +0330

    added comments of config files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,15 @@
+// Codes by mahdi tasha
+// Importing partj
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
+// Defining configs of eslint
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
 
+// Exporting the configs
 export default eslintConfig;
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,10 @@
+// Codes by mahdi tasha
+// Definning configs of tailwindcss
 const config = {
   plugins: {
     "@tailwindcss/postcss": {},
   },
 };
 
+// Exporting the configs
 export default config;
