a7fc39005269ee46cfd19c5227ccfe55bafcc031
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2025-12-13 12:22:55 +0330

    added some base for layout and home page

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+// Codes by mahdi tasha
+// Importing part
+import { RootLayoutProps } from "@/type/component";
+import { Metadata } from "next";
+import { JSX } from "react";
+
+// Defining metadata
+export const metadata: Metadata = {
+  title: "HI",
+};
+
+// Defining fonts
+
+// Creating and exporting RootLayout component as default
+export default function RootLayout({ children }: RootLayoutProps): JSX.Element {
+  // Returning JSX
+  return (
+    <html suppressHydrationWarning>
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+// Codes by mahdi tasha
+// Importing part
+import { JSX } from "react";
+
+// Creating and exporting Home page as default
+export default function HomePage(): JSX.Element {
+  // Returning JSX
+  return (
+    <div>
+      <h1>HI</h1>
+    </div>
+  );
+}
